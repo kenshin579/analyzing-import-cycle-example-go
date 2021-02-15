5eb6a657688f726c96da5cc665353162aa85912e
kenshin579 <kenshin579@hotmail.com>
2021-02-15 22:00:05 +0900

    [AIC-1] import cycle not allowed 예제

--- p1/p1.go
+++ p1/p1.go
@@ -7,11 +7,15 @@ import (
 
 type PP1 struct{}
 
+func New() *PP1 {
+	return &PP1{}
+}
+
 func (p *PP1) HelloFromP1() {
 	fmt.Println("Hello from package p1")
 }
 
 func (p *PP1) HelloFromP2Side() {
-	pp2 := p2.New(p)
+	pp2 := p2.New()
 	pp2.HelloFromP2()
 }
